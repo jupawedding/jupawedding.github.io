4cf3d0207f4d812326b9bbe1634c7110ab2c3254
Patrick <pmccreanor@loyalty.com>
2017-05-18 13:59:38 -0400

    new site

--- app/js/main.js
+++ app/js/main.js
@@ -3,25 +3,35 @@ $('.play-btn').click(function(){
 });
 
 
+// https://developers.google.com/youtube/iframe_api_reference
+
+// global variable for the player
+var player;
+
+// this function gets called when API is ready to use
+function onYouTubePlayerAPIReady() {
+  // create the global player from the specific iframe (#video)
+  player = new YT.Player('video', {
+    events: {
+      // call this function when player is ready to use
+      'onReady': onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  
+  // bind events
+  var playButton = document.getElementById("play-btn");
+  playButton.addEventListener("click", function() {
+    player.playVideo();
+  });
+}
+
+// Inject YouTube API script
 var tag = document.createElement('script');
 tag.src = "//www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-function onYouTubePlayerAPIReady() {
-	player = new YT.Player('video', {
-    	events: {
-      	
-      	'onReady': onPlayerReady
-    	}
-  	});
-}
-
-function onPlayerReady(event) {
-  
- var playButton = document.getElementById("play-btn");
-  playButton.addEventListener("click", function() {
-    player.playVideo();
-  });
-}
 
