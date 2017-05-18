eb7e369f3fd4e43bad988494c74ce16ef485bc14
Patrick <pmccreanor@loyalty.com>
2017-05-18 13:37:16 -0400

    new site

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,6 @@
-var line_animation = document.getElementById("line-container");
-var $pa = document.getElementById("pa");
-
-var arrow = document.getElementById("arrow");
-var $ju = document.getElementById("ju");
-var $date = document.getElementById("date");
-
-
-line_animation.addEventListener("animationend", revealArrow, false);
-//$ju.addEventListener("animationend", revealPa, false);
-
-
-function revealArrow(){
-	arrow.classList.remove("hide");
-	$date.classList.remove("hide");
-}
-
-function revealJu(){
-	$ju.classList.remove("hide");
-}
+$('.play-btn').click(function(){
+	$('.vinyl-image').addClass('animate');
+});
 
 
 
