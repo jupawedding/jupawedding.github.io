099e6ae57d10164a6ee741655d56e18889781a61
Patrick <pmccreanor@loyalty.com>
2017-05-18 13:45:36 -0400

    new site

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,25 @@ $('.play-btn').click(function(){
 });
 
 
+var tag = document.createElement('script');
+tag.src = "//www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+function onYouTubePlayerAPIReady() {
+	player = new YT.Player('video', {
+    	events: {
+      	
+      	'onReady': onPlayerReady
+    	}
+  	});
+}
+
+function onPlayerReady(event) {
+  
+ var playButton = document.getElementById("play-btn");
+  playButton.addEventListener("click", function() {
+    player.playVideo();
+  });
+}
 
